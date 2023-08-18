2e20b661222e52e6792b20741bc0c57b77444ae3
Vitor <vitorginza@gmail.com>
2023-08-18 19:52:02 -0300

    ajustando o js

--- Dia 4/dia4.js
+++ Dia 4/dia4.js
@@ -4,20 +4,20 @@ restauranteabrt = false
 
 
 let opçãoFome = prompt("Voce esta com fome? sim ou não")
-if (opçãoFome) {
-    opçãoFome == "sim"
+if (opçãoFome == "sim") {
+
     fome = true
 }
 
 let opçãoDinheiro = prompt("Voce esta com dinheiro hoje? sim ou não")
-if (opçãoDinheiro) {
- opçãoDinheiro == "sim"
+if ( opçãoDinheiro == "sim") {
+
  dinheiro = true    
 }
 
 let restauranteaberto = prompt("O restaurante esta aberto hoje? sim ou não")
-if (restauranteaberto) {
-    restauranteaberto == "sim"
+if (  restauranteaberto == "sim") {
+    
     restauranteabrt = true
 }
 
@@ -28,7 +28,7 @@ console.log("Hoje a janta será em casa")
     console.log("Peça um delivery")
 
  }
- if (fome && dinheiro && restauranteabrt) {
+ else{
     console.log("Hoje o jantar será no seu restaurante preferido!")
     
  }
