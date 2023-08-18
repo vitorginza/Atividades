71afd2318e072e9bdbe80546dea18498b5a2fcb1
Vitor <vitorginza@gmail.com>
2023-08-18 19:32:45 -0300

    atividade dia 4

---
+++ Dia 4/dia4.js
@@ -0,0 +1,34 @@
+fome = false
+dinheiro = false
+restauranteabrt = false
+
+
+let opçãoFome = prompt("Voce esta com fome? sim ou não")
+if (opçãoFome) {
+    opçãoFome == "sim"
+    fome = true
+}
+
+let opçãoDinheiro = prompt("Voce esta com dinheiro hoje? sim ou não")
+if (opçãoDinheiro) {
+ opçãoDinheiro == "sim"
+ dinheiro = true    
+}
+
+let restauranteaberto = prompt("O restaurante esta aberto hoje? sim ou não")
+if (restauranteaberto) {
+    restauranteaberto == "sim"
+    restauranteabrt = true
+}
+
+if(!fome  || !dinheiro){
+console.log("Hoje a janta será em casa")
+}
+ if(fome && dinheiro && restauranteabrt){
+    console.log("Peça um delivery")
+
+ }
+ if (fome && dinheiro && restauranteabrt) {
+    console.log("Hoje o jantar será no seu restaurante preferido!")
+    
+ }
