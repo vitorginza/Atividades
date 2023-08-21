fce8b566d4e918ddeac9a7bf16abf1dcb960060d
Vitor <vitorginza@gmail.com>
2023-08-21 18:29:54 -0300

    Adicionando exercicio do dia 6

---
+++ Dia 6/dia6.js
@@ -0,0 +1,20 @@
+
+//Exercicio 1
+
+let valor = parseInt(prompt("Coloque um valor:"))
+for (let i = 0; i <= valor; i++) {
+ console.log(i)
+    
+}
+
+//1.2
+for (let i = 0; i <= 50; i+=5) {
+    console.log(i)
+    
+}
+
+//1.3
+
+for (let i = 50; i > 0; i-= 5) {
+    console.log(i)
+}
