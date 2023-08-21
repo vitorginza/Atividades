4a2b32a09ec0c854d8ec415f9b04cff6380593f6
Vitor <vitorginza@gmail.com>
2023-08-21 18:30:11 -0300

    Adicionando

---
+++ Dia 6/dia6exerc2.js
@@ -0,0 +1,13 @@
+//1.4
+
+let tabuada = parseInt(prompt("Escolha um numero da tabuada"))
+
+for (let i = tabuada; i <= tabuada + 3 ; i++) {
+    console.log("Numero de entrada:" + tabuada)
+
+    for (let b = 0; b <= 10; b++) {
+        console.log("Numero:" + i+" x "+ b +" = " +(i*b)  )
+        
+    }
+    
+}
