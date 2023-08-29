512c2ea7876281a61684823f20ea6e3dc4579d0b
Vitor <vitorginza@gmail.com>
2023-08-29 09:27:22 -0300

    ajustando calculadora

--- Dia 8 calculadora/Calculadora.js
+++ Dia 8 calculadora/Calculadora.js
@@ -1,14 +1,63 @@
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function name(numero1,numero2,operador) {
+//ESCREVA SEU CÓDIGO AQUI!!
+
+function Calculadora(numero1, numero2, operador) {
     let resultado
     let num1 = parseInt(numero1)
     let num2 = parseInt(numero2)
     switch (operador) {
-        case "soma":
+        case "+":
             resultado = num1 + num2
-             break;
+            break;
+        case "-":
+            resultado = num1 - num2
+            break;
+        case "*":
+            resultado = num1 * num2
+            break;
+        case "/":
+            resultado = num1 / num2
+            break;
+        case "e":
+            resultado = num1 * num1
+            let contador = 0
+            while (contador < num2 -1) {
+              resultado = resultado * num1
+              contador++
+            }
+            break;
     }
+    if(resultado == undefined || resultado > 1000000)
+         resultado ="ERRO"
+    
+
     return resultado
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let visor = document.getElementById("visor");
+
+function adicionar(tecla) {
+  visor.value = visor.value + tecla;
+}
+
+function limpezatotal() {
+  visor.value = "";
+}
+
+function calcular() {
+  let numeros = visor.value.split(/[^0-9]/)
+  let operadores = visor.value.split(/[0-9]/)
+  let operador
+  operadores.forEach(x => {
+    if (x != "")
+      operador = x
+  });
+
+  visor.value = Calculadora(numeros[0], numeros[1], operador)
+}
